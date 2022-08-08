c2d43effa65ef223b5c046473bd6e532e516d462
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-08-07 22:52:52 -0500

    Solución al reto de la clase 7: hacer la clase y los objetos para las rutas de aprendizaje, con algunas mejoras añadidas

--- main.js
+++ main.js
@@ -16,17 +16,17 @@ natalia.aprobarCurso("Curso de Responsive Design");
 console.log(natalia);
 
 //Creando un prototipo:
-function Student(name, age, cursosAprobados){
+function Student0(name, age, cursosAprobados){
     this.name = name;
     this.age = age;
     this.cursosAprobados = cursosAprobados;
 }
 
-Student.prototype.aprobarCurso = function (nuevoCurso){
+Student0.prototype.aprobarCurso = function (nuevoCurso){
     this.cursosAprobados.push(nuevoCurso);
 }
 
-const juanita = new Student('Juanita', 25, ["Curso Definitivo de HTML y CSS", "Curso Práctico de HTML y CSS"]);
+const juanita = new Student0('Juanita', 25, ["Curso Definitivo de HTML y CSS", "Curso Práctico de HTML y CSS"]);
 juanita.aprobarCurso("Curso de Responsive Design");
 console.log(juanita);
 
@@ -59,13 +59,13 @@ class Student3 {
         instagram,
         twitter,
     }){
-        this.name = name,
-        this.age = age,
-        this.cursosAprobados = cursosAprobados,
-        this.email = email,
-        this.facebook = facebook ??= 'none',
-        this.instagram = instagram ??= 'none',
-        this.twitter = twitter ??= 'none'
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+        this.email = email;
+        this.facebook = facebook ??= 'none';
+        this.instagram = instagram ??= 'none';
+        this.twitter = twitter ??= 'none';
     }
     aprobarCurso(curso) {
         this.cursosAprobados.push(curso);
--- pooEnJS.js
+++ pooEnJS.js
@@ -1,3 +1,52 @@
+//Clase para las rutas de las escuelas: 
+
+class LearningPaths {
+    constructor({
+        name,
+        courses = []
+    }){
+        this.name = name;
+        this.courses = courses;
+    }
+    addCourse(newCourse) {
+        this.courses.push(newCourse);
+    }
+}
+
+//Escuela web:
+const escuelaWeb = new LearningPaths({
+    name: "Escuela de Desarrollo Web",
+    courses: [
+        "Curso definitivo de HTML y CSS",
+        "Curso práctico de HTML y CSS",
+        "Curso de responsive design"
+    ]
+});
+
+//Escuela videojuegos
+const escuelaVideojuegos = new LearningPaths({
+    name: "Escuela de Desarrollo de Videojuegos",
+    courses: [
+        "Curso de introducción a la producción de VGS",
+        "Curso de unreal engine",
+        "Curso de unity 3D"
+    ]
+});
+
+//Escuela Data science
+const escuelaDataScience = new LearningPaths({
+    name: "Escuela de Data Science",
+    courses: [
+        "Curso DataBusiness",
+        "Curso DataViz",
+        "Curso de Tableau"
+    ]
+})
+
+// ----------------------------------------------------------------- //
+
+//Clase para los estudiantes:
+
 class Student {
     constructor({
         name,
@@ -7,7 +56,8 @@ class Student {
         instagram = undefined,
         facebook = undefined,
         approvedCourses = [],
-        learningPaths = []
+        learningPaths = [],
+        personalPath
     }){
         this.name = name;
         this.email = email;
@@ -19,23 +69,43 @@ class Student {
         };
         this.approvedCourses = approvedCourses;
         this.learningPaths = learningPaths;
+        this.personalPath = personalPath;
 
     }
 }
 
+//Estudiantes:
 const juan2 = new Student ({
     name: "JuanDC",
     username: "juandc",
     email: "juanito@juanito.com",
-    twitter: "fjuandc"
+    twitter: "fjuandc",
+    learningPaths: [
+        escuelaWeb,
+        escuelaDataScience
+    ]
 })
+
+console.log(juan2);
 
 const miguelito2 = new Student ({
     name: "Miguelito",
     username: "miguelito_feliz",
     email: "miguelito@juanito.com",
-    instagram: "miguelito_feliz"
+    instagram: "miguelito_feliz",
+    learningPaths: [
+        escuelaVideojuegos,
+        escuelaDataScience
+    ],
+    personalPath: new LearningPaths ({
+        name: 'Mis cursos',
+        courses: [
+            "Curso de POO", 
+            "Curso de intro a la terminal", 
+            "Curso de finanzas personales"
+        ]
+    })
 })
 
-console.log(juan2);
+miguelito2.personalPath.addCourse("Curso de historia de los lenguajes de programación");
 console.log(miguelito2);
